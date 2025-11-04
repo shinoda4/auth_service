70490136f0d394276d29f2148af02a82ea166617
shinoda4 <lindesong666@gmail.com>
2025-11-04 20:09:02 +0800

    add collect static.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 cd auth_service
 uv run python manage.py migrate
+uv run python manage.py collectstatic --noinput
 uv run gunicorn auth_service.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
